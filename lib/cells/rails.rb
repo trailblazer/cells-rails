a078433d3df497cc31601f6a44c8592cf6347a39
Nick Sutterer <apotonick@gmail.com>
2016-05-02 11:38:54 +1000

    add generators.

--- lib/cells/rails.rb
+++ lib/cells/rails.rb
@@ -1,13 +1,8 @@
 require "cells/rails/version"
 require "cells"
 
-require "cell/rails/collection"
-require "cell/rails/constant_for"
-
-Cell::Collection.send :include, Cell::RailsExtension::Collection
-Cell::ViewModel.send :include, Cell::RailsExtension::ConstantFor
-
 require "cell/caching/notification"
+require "cell/rails"
 require "cell/railtie"
 
 module Cell
