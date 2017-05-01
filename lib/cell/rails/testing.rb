c23baa45a27a670e0c0815d1125aed6b922776e6
Georg Leciejewski <gl@salesking.eu>
2017-05-01 14:24:17 +0200

    in rails 5.1 ActionController::TestRequest.create needs controller class as param

--- lib/cell/rails/testing.rb
+++ lib/cell/rails/testing.rb
@@ -12,9 +12,13 @@ module Cell
       end
 
       def action_controller_test_request
-        if ::Rails.version.start_with?('5')
-          ::ActionController::TestRequest.create
-        else
+        if ::Rails::VERSION::MAJOR >= 5
+          if ::Rails::VERSION::MINOR >= 1 # ~> 5.1
+            ::ActionController::TestRequest.create(self.class.controller_class)
+          else # 5.0
+            ::ActionController::TestRequest.create
+          end
+        else #< 5
           ::ActionController::TestRequest.new
         end
       end
