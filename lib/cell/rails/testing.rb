e6eee8c86ed77d4e80d46c3f8c07503eaae71344
Tiago Amaro <tiagopadrela@gmail.com>
2017-05-09 18:32:23 -0300

    use controller class from parameter instead of context

--- lib/cell/rails/testing.rb
+++ lib/cell/rails/testing.rb
@@ -6,15 +6,15 @@ module Cell
         return unless controller_class
 
         controller_class.new.tap do |ctl|
-          ctl.request = action_controller_test_request
+          ctl.request = action_controller_test_request(controller_class)
           ctl.instance_variable_set :@routes, ::Rails.application.routes.url_helpers
         end
       end
 
-      def action_controller_test_request
+      def action_controller_test_request(controller_class)
         if ::Rails::VERSION::MAJOR >= 5
           if ::Rails::VERSION::MINOR >= 1 # ~> 5.1
-            ::ActionController::TestRequest.create(self.class.controller_class)
+            ::ActionController::TestRequest.create(controller_class)
           else # 5.0
             ::ActionController::TestRequest.create
           end
